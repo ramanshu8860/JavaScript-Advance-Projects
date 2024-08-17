8844f2cd433cedc627f28ca25f81ccd5b994f020
Ramasnhu Yadav <ramanshu8860@gmail.com>
2024-08-17 08:51:01 +0530

    second-Commit-Almost-Final-Code

--- CountryProject/App.js
+++ CountryProject/App.js
@@ -1,23 +1,60 @@
 //ab hume data fetch karna hai backend aur show karna hai
 //Study Fetch
-fetch('https://restcountries.com/v3.1/all').then(
-    (response) => response.json()
-).then((data) => data.forEach((country) => {
-    console.log(country)
-    const countryCard = document.createElement('a');
-    countryCard.classList.add('countries-card');
-    const cardHTML = `<img src="${country.flags.svg}" alt="flag">
-                    <div class="card-text"> 
-                        <h3 class = "card-title">${country.name.common}</h3>
-                        <p><b>Population: </b>${country.population.toLocaleString('en-In')}</p>
-                        <p><b>Region: </b>${country.region}</p>
-                        <p><b>Capital: </b>${country.capital[0]}</p>
-                    </div>`
-    countryCard.innerHTML = cardHTML;
-    countriesContainer.append(countryCard);
-}))
-
+//command + shift + V -> preview readme.md file
 const countriesContainer = document.querySelector('.countries-container')
+const filterByRegion = document.querySelector('.filter-by-region')
+const searchInput = document.querySelector('.search-container input')
+const themeChanger = document.querySelector('.theme-changer')
+
+let allCountriesData
+
+fetch('https://restcountries.com/v3.1/all')
+  .then((res) => res.json())
+  .then((data) => {
+    renderCountries(data)
+    allCountriesData = data
+  })
+
+filterByRegion.addEventListener('change', (e) => {
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+    .then((res) => res.json())
+    .then(renderCountries)
+})
+
+function renderCountries(data) {
+  countriesContainer.innerHTML = ''
+  data.forEach((country) => {
+    const countryCard = document.createElement('a')
+    countryCard.classList.add('country-card')
+    countryCard.href = `country.html?name=${country.name.common}`
+    countryCard.innerHTML = `
+          <img src="${country.flags.svg}" alt="${country.name.common} flag" />
+          <div class="card-text">
+              <h3 class="card-title">${country.name.common}</h3>
+              <p><b>Population: </b>${country.population.toLocaleString(
+                'en-IN'
+              )}</p>
+              <p><b>Region: </b>${country.region}</p>
+              <p><b>Capital: </b>${country.capital?.[0]}</p>
+          </div>
+  `
+    countriesContainer.append(countryCard)
+  })
+}
+
+
+searchInput.addEventListener('input',  (e) => {
+  const filteredCountries = allCountriesData.filter((country) => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+  renderCountries(filteredCountries)
+})
+
+themeChanger.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+})
+
+
+
+
 
 
 // const cardImage = document.createElement('img');
@@ -36,3 +73,10 @@ const countriesContainer = document.querySelector('.countries-container')
 //                     </div>`
 // countryCard.innerHTML = cardHTML;
 // countriesContainer.append(countryCard);
+
+
+
+//Abhi Humne Link nahi banayi hai first commit mei
+//Now the task is hume link banana hai click kare to details aa jaye saari country ki
+
+
